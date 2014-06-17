1013468c5a90873ff0749568b9ae9854ea0114b5
Henk Mollema <henkmollema@gmail.com>
2014-06-17 20:23:19 +0200

    More testing.

--- src/Dommel.ConsoleTest/Program.cs
+++ src/Dommel.ConsoleTest/Program.cs
@@ -16,7 +16,7 @@ namespace Dommel.ConsoleTest
     {
         public ProductMap()
         {
-            Map(p => p.Id).ToColumn("autID");
+            //Map(p => p.Id).ToColumn("autID");
             Map(p => p.Name).ToColumn("strName");
         }
     }
@@ -29,54 +29,26 @@ namespace Dommel.ConsoleTest
         }
     }
 
-    public class CustomKeyPropertyResolver : SqlMapperExtensions.IKeyPropertyResolver
-    {
-        public PropertyInfo ResolveKeyProperty(Type type)
-        {
-            return null;
-        }
-    }
-
     public class Program
     {
         public static void Main(string[] args)
         {
-            using (var con = new SqlConnection("Data Source=.\\sql2012;Initial Catalog=DapperTest;Integrated Security=True"))
+            using (var con = new SqlConnection("Data Source=sql2012;Initial Catalog=DapperTest;Integrated Security=True"))
             {
                 SqlMapperExtensions.SetTableNameResolver(new CustomTableNameResolver());
                 FluentMapper.Intialize(c => c.AddMap(new ProductMap()));
 
 
-                var sw = Stopwatch.StartNew();
-                var p = con.Get<Product>(1);
-                sw.Stop();
+                for (int i = 1; i <= 7; i++)
+                {
+                    var sw = Stopwatch.StartNew();
+                    var p = con.Get<Product>(i);
+                    sw.Stop();
 
-                Console.WriteLine(p.Id);
-                Console.WriteLine(p.Name);
-
-                Console.WriteLine("Query executed in {0}ms", sw.Elapsed.TotalMilliseconds);
-
-                Console.WriteLine("");
-
-                var sw2 = Stopwatch.StartNew();
-                var p2 = con.Get<Product>(2);
-                sw2.Stop();
-
-                Console.WriteLine(p2.Id);
-                Console.WriteLine(p2.Name);
-
-                Console.WriteLine("Query executed in {0}ms", sw2.Elapsed.TotalMilliseconds);
-
-                Console.WriteLine("");
-
-                var sw3 = Stopwatch.StartNew();
-                var p3 = con.Get<Product>(3);
-                sw3.Stop();
-
-                Console.WriteLine(p3.Id);
-                Console.WriteLine(p3.Name);
-
-                Console.WriteLine("Query executed in {0}ms", sw3.Elapsed.TotalMilliseconds);
+                    Console.WriteLine("{0}: {1}", p.Id, p.Name);
+                    Console.WriteLine("Query executed in {0}ms", sw.Elapsed.TotalMilliseconds);
+                    Console.WriteLine("");
+                }
             }
 
             Console.ReadKey();
