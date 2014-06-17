7e1fb6803673428e268f500b613645d9cb6775ce
HenkMollema <henkmollema@gmail.com>
2014-06-17 16:47:19 +0200

    Added Console app for testing

---
+++ src/Dommel.ConsoleTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dommel.ConsoleTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(args);
+            Console.ReadKey();
+        }
+    }
+}
