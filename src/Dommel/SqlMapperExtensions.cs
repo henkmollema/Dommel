93cc626ceb16f80a516f6312bcf11bbc00d9ba54
HenkMollema <henkmollema@gmail.com>
2014-06-17 13:58:29 +0200

    Added solution, project and SqlMapperExtensions file

---
+++ src/Dommel/SqlMapperExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace Dommel
+{
+    public static class SqlMapperExtensions
+    {
+        public static T Get<T>(this IDbConnection con, object id)
+        {
+            return default(T);
+        }
+    }
+}
