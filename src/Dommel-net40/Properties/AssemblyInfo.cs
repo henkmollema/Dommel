49936e74b427878297dba8325056d65fb5745d7c
Henk Mollema <henkmollema@gmail.com>
2015-05-29 14:39:22 +0200

    Added DNX project

---
+++ src/Dommel-net40/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Dommel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dommel")]
+[assembly: AssemblyCopyright("Copyright © 2014 Henk Mollema")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6c76538f-9fb2-4789-972c-d72e2f468462")]
+
+[assembly: AssemblyVersion("1.4.0")]
