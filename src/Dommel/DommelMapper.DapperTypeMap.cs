2f359be0bd844b1b8beb7b18e0e31253d0941679
Henk Mollema <henkmollema@gmail.com>
2018-03-20 21:33:31 +0100

    Add a [Column] attribute Dapper type mapping
    A custom Dapper type mapping strategy to support [Column] attributes on properties.

---
+++ src/Dommel/DommelMapper.DapperTypeMap.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection;
+using Dapper;
+
+namespace Dommel
+{
+    /// <summary>
+    /// Simple CRUD operations for Dapper.
+    /// </summary>
+    public static partial class DommelMapper
+    {
+        private static readonly ConcurrentDictionary<string, PropertyInfo> _columnNameCache = new ConcurrentDictionary<string, PropertyInfo>();
+
+        static DommelMapper()
+        {
+            SqlMapper.TypeMapProvider = type => CreateMap(type);
+
+            SqlMapper.ITypeMap CreateMap(Type t) => new CustomPropertyTypeMap(t,
+                (type, columnName) =>
+                {
+                    var cacheKey = type + columnName;
+                    if (!_columnNameCache.TryGetValue(cacheKey, out var propertyInfo))
+                    {
+                        propertyInfo = type.GetProperties().FirstOrDefault(p => p.GetCustomAttribute<ColumnAttribute>()?.Name == columnName || p.Name == columnName);
+                        _columnNameCache.TryAdd(cacheKey, propertyInfo);
+                    }
+
+                    return propertyInfo;
+                });
+        }
+    }
+}
