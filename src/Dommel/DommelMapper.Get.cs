1680f5b9166e10aeef865482b17e46f5f9c5f687
Henk Mollema <henkmollema@gmail.com>
2018-03-21 14:59:44 +0100

    Add more transaction parameters

--- src/Dommel/DommelMapper.Get.cs
+++ src/Dommel/DommelMapper.Get.cs
@@ -18,12 +18,13 @@ namespace Dommel
         /// <typeparam name="TEntity">The type of the entity.</typeparam>
         /// <param name="connection">The connection to the database. This can either be open or closed.</param>
         /// <param name="id">The id of the entity in the database.</param>
+        /// <param name="transaction">Optional transaction for the command.</param>
         /// <returns>The entity with the corresponding id.</returns>
-        public static TEntity Get<TEntity>(this IDbConnection connection, object id) where TEntity : class
+        public static TEntity Get<TEntity>(this IDbConnection connection, object id, IDbTransaction transaction = null) where TEntity : class
         {
             var sql = BuildGetById(typeof(TEntity), id, out var parameters);
             LogQuery<TEntity>(sql);
-            return connection.QueryFirstOrDefault<TEntity>(sql, parameters);
+            return connection.QueryFirstOrDefault<TEntity>(sql, parameters, transaction);
         }
 
         /// <summary>
@@ -32,12 +33,13 @@ namespace Dommel
         /// <typeparam name="TEntity">The type of the entity.</typeparam>
         /// <param name="connection">The connection to the database. This can either be open or closed.</param>
         /// <param name="id">The id of the entity in the database.</param>
+        /// <param name="transaction">Optional transaction for the command.</param>
         /// <returns>The entity with the corresponding id.</returns>
-        public static Task<TEntity> GetAsync<TEntity>(this IDbConnection connection, object id) where TEntity : class
+        public static Task<TEntity> GetAsync<TEntity>(this IDbConnection connection, object id, IDbTransaction transaction = null) where TEntity : class
         {
             var sql = BuildGetById(typeof(TEntity), id, out var parameters);
             LogQuery<TEntity>(sql);
-            return connection.QueryFirstOrDefaultAsync<TEntity>(sql, parameters);
+            return connection.QueryFirstOrDefaultAsync<TEntity>(sql, parameters, transaction);
         }
 
         private static string BuildGetById(Type type, object id, out DynamicParameters parameters)
@@ -67,12 +69,13 @@ namespace Dommel
         /// A value indicating whether the result of the query should be executed directly,
         /// or when the query is materialized (using <c>ToList()</c> for example).
         /// </param>
+        /// <param name="transaction">Optional transaction for the command.</param>
         /// <returns>A collection of entities of type <typeparamref name="TEntity"/>.</returns>
-        public static IEnumerable<TEntity> GetAll<TEntity>(this IDbConnection connection, bool buffered = true) where TEntity : class
+        public static IEnumerable<TEntity> GetAll<TEntity>(this IDbConnection connection, IDbTransaction transaction = null, bool buffered = true) where TEntity : class
         {
             var sql = BuildGetAllQuery(typeof(TEntity));
             LogQuery<TEntity>(sql);
-            return connection.Query<TEntity>(sql, buffered: buffered);
+            return connection.Query<TEntity>(sql, transaction: transaction, buffered: buffered);
         }
 
         /// <summary>
@@ -80,20 +83,20 @@ namespace Dommel
         /// </summary>
         /// <typeparam name="TEntity">The type of the entity.</typeparam>
         /// <param name="connection">The connection to the database. This can either be open or closed.</param>
+        /// <param name="transaction">Optional transaction for the command.</param>
         /// <returns>A collection of entities of type <typeparamref name="TEntity"/>.</returns>
-        public static Task<IEnumerable<TEntity>> GetAllAsync<TEntity>(this IDbConnection connection) where TEntity : class
+        public static Task<IEnumerable<TEntity>> GetAllAsync<TEntity>(this IDbConnection connection, IDbTransaction transaction = null) where TEntity : class
         {
             var sql = BuildGetAllQuery(typeof(TEntity));
             LogQuery<TEntity>(sql);
-            return connection.QueryAsync<TEntity>(sql);
+            return connection.QueryAsync<TEntity>(sql, transaction: transaction);
         }
 
         private static string BuildGetAllQuery(Type type)
         {
             if (!_getAllQueryCache.TryGetValue(type, out var sql))
             {
-                var tableName = Resolvers.Table(type);
-                sql = $"select * from {tableName}";
+                sql = "select * from " + Resolvers.Table(type); ;
                 _getAllQueryCache.TryAdd(type, sql);
             }
 
