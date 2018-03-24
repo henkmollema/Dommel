a643af79b5bcd46a46ebcd5eeed6745740b65112
Henk Mollema <henkmollema@gmail.com>
2018-03-24 16:52:46 +0100

    Finish single GetAsync with automatic multimap

--- src/Dommel/DommelMapper.AutoMultiMap.cs
+++ src/Dommel/DommelMapper.AutoMultiMap.cs
@@ -20,10 +20,13 @@ namespace Dommel
         /// <param name="id">The id of the entity in the database.</param>
         /// <param name="transaction">Optional transaction for the command.</param>
         /// <returns>The entity with the corresponding id joined with the specified types.</returns>
-        public static async Task<TReturn> GetAsync<T1, T2, TReturn>(this IDbConnection connection, object id, IDbTransaction transaction = null) where TReturn : class
-        {
-            return (await MultiMapAsync<T1, T2, DontMap, DontMap, DontMap, DontMap, DontMap, TReturn>(connection, CreateMap<T1, T2, TReturn>(), id)).FirstOrDefault();
-        }
+        public static async Task<TReturn> GetAsync<T1, T2, TReturn>(this IDbConnection connection, object id, IDbTransaction transaction = null) where T1 : TReturn
+            => (await MultiMapAsync<T1, T2, DontMap, DontMap, DontMap, DontMap, DontMap, TReturn>(
+                    connection,
+                    CreateMapDelegate<T1, T2, DontMap, DontMap, DontMap, DontMap, DontMap, TReturn>(),
+                    id,
+                    transaction))
+                .FirstOrDefault();
 
         /// <summary>
         /// Retrieves the automatically mapped entity of type <typeparamref name="TReturn"/> with the specified <paramref name="id"/>
@@ -37,10 +40,13 @@ namespace Dommel
         /// <param name="id">The id of the entity in the database.</param>
         /// <param name="transaction">Optional transaction for the command.</param>
         /// <returns>The entity with the corresponding id joined with the specified types.</returns>
-        public static async Task<TReturn> GetAsync<T1, T2, T3, TReturn>(this IDbConnection connection, object id, IDbTransaction transaction = null) where TReturn : class
-        {
-            return (await MultiMapAsync<T1, T2, T3, DontMap, DontMap, DontMap, DontMap, TReturn>(connection, CreateMap<T1, T2, T3, TReturn>(), id)).FirstOrDefault();
-        }
+        public static async Task<TReturn> GetAsync<T1, T2, T3, TReturn>(this IDbConnection connection, object id, IDbTransaction transaction = null) where T1 : TReturn
+            => (await MultiMapAsync<T1, T2, T3, DontMap, DontMap, DontMap, DontMap, TReturn>(
+                    connection,
+                    CreateMapDelegate<T1, T2, T3, DontMap, DontMap, DontMap, DontMap, TReturn>(),
+                    id,
+                    transaction))
+                .FirstOrDefault();
 
         /// <summary>
         /// Retrieves the automatically mapped entity of type <typeparamref name="TReturn"/> with the specified <paramref name="id"/>
@@ -55,63 +61,199 @@ namespace Dommel
         /// <param name="id">The id of the entity in the database.</param>
         /// <param name="transaction">Optional transaction for the command.</param>
         /// <returns>The entity with the corresponding id joined with the specified types.</returns>
-        public static async Task<TReturn> GetAsync<T1, T2, T3, T4, TReturn>(this IDbConnection connection, object id, IDbTransaction transaction = null) where TReturn : class
-        {
-            return (await MultiMapAsync<T1, T2, T3, T4, DontMap, DontMap, DontMap, TReturn>(connection, CreateMap<T1, T2, T3, T4, TReturn>(), id)).FirstOrDefault();
-        }
+        public static async Task<TReturn> GetAsync<T1, T2, T3, T4, TReturn>(this IDbConnection connection, object id, IDbTransaction transaction = null) where T1 : TReturn
+            => (await MultiMapAsync<T1, T2, T3, T4, DontMap, DontMap, DontMap, TReturn>(
+                    connection,
+                    CreateMapDelegate<T1, T2, T3, T4, DontMap, DontMap, DontMap, TReturn>(),
+                    id,
+                    transaction))
+                .FirstOrDefault();
 
-        private static Func<T1, T2, TReturn> CreateMap<T1, T2, TReturn>()
+        /// <summary>
+        /// Retrieves the automatically mapped entity of type <typeparamref name="TReturn"/> with the specified <paramref name="id"/>
+        /// joined with the types specified as type parameters.
+        /// </summary>
+        /// <typeparam name="T1">The first type parameter. This is the source entity.</typeparam>
+        /// <typeparam name="T2">The second type parameter.</typeparam>
+        /// <typeparam name="T3">The third type parameter.</typeparam>
+        /// <typeparam name="T4">The fourth type parameter.</typeparam>
+        /// <typeparam name="T5">The fifth type parameter.</typeparam>
+        /// <typeparam name="TReturn">The return type parameter.</typeparam>
+        /// <param name="connection">The connection to the database. This can either be open or closed.</param>
+        /// <param name="id">The id of the entity in the database.</param>
+        /// <param name="transaction">Optional transaction for the command.</param>
+        /// <returns>The entity with the corresponding id joined with the specified types.</returns>
+        public static async Task<TReturn> GetAsync<T1, T2, T3, T4, T5, TReturn>(this IDbConnection connection, object id, IDbTransaction transaction = null) where T1 : TReturn
+            => (await MultiMapAsync<T1, T2, T3, T4, T5, DontMap, DontMap, TReturn>(
+                    connection,
+                    CreateMapDelegate<T1, T2, T3, T4, T5, DontMap, DontMap, TReturn>(),
+                    id,
+                    transaction))
+                .FirstOrDefault();
+
+        /// <summary>
+        /// Retrieves the automatically mapped entity of type <typeparamref name="TReturn"/> with the specified <paramref name="id"/>
+        /// joined with the types specified as type parameters.
+        /// </summary>
+        /// <typeparam name="T1">The first type parameter. This is the source entity.</typeparam>
+        /// <typeparam name="T2">The second type parameter.</typeparam>
+        /// <typeparam name="T3">The third type parameter.</typeparam>
+        /// <typeparam name="T4">The fourth type parameter.</typeparam>
+        /// <typeparam name="T5">The fifth type parameter.</typeparam>
+        /// <typeparam name="T6">The sixth type parameter.</typeparam>
+        /// <typeparam name="TReturn">The return type parameter.</typeparam>
+        /// <param name="connection">The connection to the database. This can either be open or closed.</param>
+        /// <param name="id">The id of the entity in the database.</param>
+        /// <param name="transaction">Optional transaction for the command.</param>
+        /// <returns>The entity with the corresponding id joined with the specified types.</returns>
+        public static async Task<TReturn> GetAsync<T1, T2, T3, T4, T5, T6, TReturn>(this IDbConnection connection, object id, IDbTransaction transaction = null) where T1 : TReturn
+            => (await MultiMapAsync<T1, T2, T3, T4, T5, T6, DontMap, TReturn>(
+                    connection,
+                    CreateMapDelegate<T1, T2, T3, T4, T5, T6, DontMap, TReturn>(),
+                    id,
+                    transaction))
+                .FirstOrDefault();
+
+        /// <summary>
+        /// Retrieves the automatically mapped entity of type <typeparamref name="TReturn"/> with the specified <paramref name="id"/>
+        /// joined with the types specified as type parameters.
+        /// </summary>
+        /// <typeparam name="T1">The first type parameter. This is the source entity.</typeparam>
+        /// <typeparam name="T2">The second type parameter.</typeparam>
+        /// <typeparam name="T3">The third type parameter.</typeparam>
+        /// <typeparam name="T4">The fourth type parameter.</typeparam>
+        /// <typeparam name="T5">The fifth type parameter.</typeparam>
+        /// <typeparam name="T6">The sixth type parameter.</typeparam>
+        /// <typeparam name="T7">The seventh type parameter.</typeparam>
+        /// <typeparam name="TReturn">The return type parameter.</typeparam>
+        /// <param name="connection">The connection to the database. This can either be open or closed.</param>
+        /// <param name="id">The id of the entity in the database.</param>
+        /// <param name="transaction">Optional transaction for the command.</param>
+        /// <returns>The entity with the corresponding id joined with the specified types.</returns>
+        public static async Task<TReturn> GetAsync<T1, T2, T3, T4, T5, T6, T7, TReturn>(this IDbConnection connection, object id, IDbTransaction transaction = null) where T1 : TReturn
+            => (await MultiMapAsync<T1, T2, T3, T4, T5, T6, T7, TReturn>(
+                    connection,
+                    CreateMapDelegate<T1, T2, T3, T4, T5, T6, T7, TReturn>(),
+                    id,
+                    transaction))
+                .FirstOrDefault();
+
+        private static Delegate CreateMapDelegate<T1, T2, T3, T4, T5, T6, T7, TReturn>()
+            where T1 : TReturn
         {
-            var result = default(T1);
-            return (target, t2) =>
+            var includeTypes = new[]
             {
-                if (result == null)
-                {
-                    result = target;
-                }
+                typeof(T1),
+                typeof(T2),
+                typeof(T3),
+                typeof(T4),
+                typeof(T5),
+                typeof(T6),
+                typeof(T7)
+            }
+            .Where(t => t != typeof(DontMap))
+            .ToArray();
 
-                var targetProperties = typeof(T1).GetProperties();
-                MapValueToTarget(targetProperties, result, t2);
-
-                return (TReturn)(object)result;
-            };
-        }
-
-        private static Func<T1, T2, T3, TReturn> CreateMap<T1, T2, T3, TReturn>()
-        {
             var result = default(T1);
-            return (target, t2, t3) =>
+            var targetProperties = typeof(T1).GetProperties();
+
+            switch (includeTypes.Length)
             {
-                if (result == null)
-                {
-                    result = target;
-                }
+                case 2:
+                    Func<T1, T2, TReturn> f2 = (target, t2) =>
+                    {
+                        if (result == null)
+                        {
+                            result = target;
+                        }
 
-                var targetProperties = typeof(T1).GetProperties();
-                MapValueToTarget(targetProperties, result, t2);
-                MapValueToTarget(targetProperties, result, t3);
+                        MapValueToTarget(targetProperties, result, t2);
+                        return result;
+                    };
+                    return f2;
 
-                return (TReturn)(object)result;
-            };
-        }
+                case 3:
+                    Func<T1, T2, T3, TReturn> f3 = (target, t2, t3) =>
+                    {
+                        if (result == null)
+                        {
+                            result = target;
+                        }
 
-        private static Func<T1, T2, T3, T4, TReturn> CreateMap<T1, T2, T3, T4, TReturn>()
-        {
-            var result = default(T1);
-            return (target, t2, t3, t4) =>
-            {
-                if (result == null)
-                {
-                    result = target;
-                }
+                        MapValueToTarget(targetProperties, result, t2);
+                        MapValueToTarget(targetProperties, result, t3);
+                        return result;
+                    };
+                    return f3;
 
-                var targetProperties = typeof(T1).GetProperties();
-                MapValueToTarget(targetProperties, result, t2);
-                MapValueToTarget(targetProperties, result, t3);
-                MapValueToTarget(targetProperties, result, t4);
+                case 4:
+                    Func<T1, T2, T3, T4, TReturn> f4 = (target, t2, t3, t4) =>
+                    {
+                        if (result == null)
+                        {
+                            result = target;
+                        }
 
-                return (TReturn)(object)result;
-            };
+                        MapValueToTarget(targetProperties, result, t2);
+                        MapValueToTarget(targetProperties, result, t3);
+                        MapValueToTarget(targetProperties, result, t4);
+                        return result;
+                    };
+                    return f4;
+
+                case 5:
+                    Func<T1, T2, T3, T4, T5, TReturn> f5 = (target, t2, t3, t4, t5) =>
+                    {
+                        if (result == null)
+                        {
+                            result = target;
+                        }
+
+                        MapValueToTarget(targetProperties, result, t2);
+                        MapValueToTarget(targetProperties, result, t3);
+                        MapValueToTarget(targetProperties, result, t4);
+                        MapValueToTarget(targetProperties, result, t5);
+                        return result;
+                    };
+                    return f5;
+
+                case 6:
+                    Func<T1, T2, T3, T4, T5, T6, TReturn> f6 = (target, t2, t3, t4, t5, t6) =>
+                    {
+                        if (result == null)
+                        {
+                            result = target;
+                        }
+
+                        MapValueToTarget(targetProperties, result, t2);
+                        MapValueToTarget(targetProperties, result, t3);
+                        MapValueToTarget(targetProperties, result, t4);
+                        MapValueToTarget(targetProperties, result, t5);
+                        MapValueToTarget(targetProperties, result, t6);
+                        return result;
+                    };
+                    return f6;
+
+                case 7:
+                    Func<T1, T2, T3, T4, T5, T6, T7, TReturn> f7 = (target, t2, t3, t4, t5, t6, t7) =>
+                    {
+                        if (result == null)
+                        {
+                            result = target;
+                        }
+
+                        MapValueToTarget(targetProperties, result, t2);
+                        MapValueToTarget(targetProperties, result, t3);
+                        MapValueToTarget(targetProperties, result, t4);
+                        MapValueToTarget(targetProperties, result, t5);
+                        MapValueToTarget(targetProperties, result, t6);
+                        MapValueToTarget(targetProperties, result, t7);
+                        return result;
+                    };
+                    return f7;
+            }
+
+            throw new InvalidOperationException($"Invalid amount of include types: {includeTypes.Length}.");
         }
 
         private static void MapValueToTarget<T>(PropertyInfo[] props, object target, T instance)
@@ -134,7 +276,7 @@ namespace Dommel
                     var list = new List<T> { instance };
                     prop.SetValue(target, list);
                 }
-                else if (value != null && value is ICollection<T> collection)
+                else if (value is ICollection<T> collection)
                 {
                     collection.Add(instance);
                 }
