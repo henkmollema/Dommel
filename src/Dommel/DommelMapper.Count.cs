adfdeb948b21607e12bcf8f3c8b28509bfd06a8c
Henk Mollema <henkmollema@gmail.com>
2018-03-24 17:37:10 +0100

    Add transaction parameter to Count methods

--- src/Dommel/DommelMapper.Count.cs
+++ src/Dommel/DommelMapper.Count.cs
@@ -9,7 +9,7 @@ namespace Dommel
 {
     public static partial class DommelMapper
     {
-        private static readonly ConcurrentDictionary<Type, string> _getCountCache = new ConcurrentDictionary<Type, string>();
+        private static readonly ConcurrentDictionary<Type, string> _countQueryCache = new ConcurrentDictionary<Type, string>();
 
         /// <summary>
         /// Returns the number of entities matching the specified predicate.
@@ -17,12 +17,13 @@ namespace Dommel
         /// <typeparam name="TEntity">The type of the entity.</typeparam>
         /// <param name="connection">The connection to the database. This can either be open or closed.</param>
         /// <param name="predicate">A predicate to filter the results.</param>
+        /// <param name="transaction">Optional transaction for the command.</param>
         /// <returns>The number of entities matching the specified predicate.</returns>
-        public static long Count<TEntity>(this IDbConnection connection, Expression<Func<TEntity, bool>> predicate)
+        public static long Count<TEntity>(this IDbConnection connection, Expression<Func<TEntity, bool>> predicate, IDbTransaction transaction = null)
         {
             var sql = BuildCountSql(predicate, out var parameters);
             LogQuery<TEntity>(sql);
-            return connection.ExecuteScalar<long>(sql, parameters);
+            return connection.ExecuteScalar<long>(sql, parameters, transaction);
         }
 
         /// <summary>
@@ -31,22 +32,23 @@ namespace Dommel
         /// <typeparam name="TEntity">The type of the entity.</typeparam>
         /// <param name="connection">The connection to the database. This can either be open or closed.</param>
         /// <param name="predicate">A predicate to filter the results.</param>
+        /// <param name="transaction">Optional transaction for the command.</param>
         /// <returns>The number of entities matching the specified predicate.</returns>
-        public static Task<long> CountAsync<TEntity>(this IDbConnection connection, Expression<Func<TEntity, bool>> predicate)
+        public static Task<long> CountAsync<TEntity>(this IDbConnection connection, Expression<Func<TEntity, bool>> predicate, IDbTransaction transaction = null)
         {
             var sql = BuildCountSql(predicate, out var parameters);
             LogQuery<TEntity>(sql);
-            return connection.ExecuteScalarAsync<long>(sql, parameters);
+            return connection.ExecuteScalarAsync<long>(sql, parameters, transaction);
         }
 
         private static string BuildCountSql<TEntity>(Expression<Func<TEntity, bool>> predicate, out DynamicParameters parameters)
         {
             var type = typeof(TEntity);
-            if (!_getCountCache.TryGetValue(type, out var sql))
+            if (!_countQueryCache.TryGetValue(type, out var sql))
             {
                 var tableName = Resolvers.Table(type);
                 sql = $"select count(*) from {tableName}";
-                _getCountCache.TryAdd(type, sql);
+                _countQueryCache.TryAdd(type, sql);
             }
 
             sql += new SqlExpression<TEntity>()
