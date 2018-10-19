b48561fc78bd98169ed5a69ebb6dd492e3eb79d9
Henk Mollema <henkmollema@gmail.com>
2018-10-19 16:58:39 +0200

    Add ResolveKeyProperty extension methods for backwards compat

--- src/Dommel/DommelMapper.IKeyPropertyResolver.cs
+++ src/Dommel/DommelMapper.IKeyPropertyResolver.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
+using static Dommel.DommelMapper;
 
 namespace Dommel
 {
@@ -26,5 +28,30 @@ namespace Dommel
             /// <returns>A collection of <see cref="PropertyInfo"/> instances of the key properties of <paramref name="type"/>.</returns>
             PropertyInfo[] ResolveKeyProperties(Type type, out bool isIdentity);
         }
+    }
+
+    /// <summary>
+    /// Extensions for <see cref="IKeyPropertyResolver"/>.
+    /// </summary>
+    public static class KeyPropertyResolverExtensions
+    {
+        /// <summary>
+        /// Resolves the single key property for the specified type.
+        /// </summary>
+        /// <param name="keyPropertyResolver">The <see cref="IKeyPropertyResolver"/>.</param>
+        /// <param name="type">The type to resolve the key property for.</param>
+        /// <returns>A <see cref="PropertyInfo"/> instance of the key property of <paramref name="type"/>.</returns>
+        public static PropertyInfo ResolveKeyProperty(this IKeyPropertyResolver keyPropertyResolver, Type type)
+            => keyPropertyResolver.ResolveKeyProperties(type).FirstOrDefault();
+
+        /// <summary>
+        /// Resolves the single key property for the specified type.
+        /// </summary>
+        /// <param name="keyPropertyResolver">The <see cref="IKeyPropertyResolver"/>.</param>
+        /// <param name="type">The type to resolve the key property for.</param>
+        /// <param name="isIdentity">Indicates whether the key properties are identity properties.</param>
+        /// <returns>A <see cref="PropertyInfo"/> instance of the key property of <paramref name="type"/>.</returns>
+        public static PropertyInfo ResolveKeyProperty(this IKeyPropertyResolver keyPropertyResolver, Type type, out bool isIdentity) =>
+            keyPropertyResolver.ResolveKeyProperties(type, out isIdentity).FirstOrDefault();
     }
 }
