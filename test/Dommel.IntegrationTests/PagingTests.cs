a3744c1e1a166097c07f981e25290ca946e4f83a
Henk Mollema <henkmollema@gmail.com>
2018-11-13 22:33:11 +0100

    Don't add null instances when multi mapping

--- test/Dommel.IntegrationTests/PagingTests.cs
+++ test/Dommel.IntegrationTests/PagingTests.cs
@@ -1,27 +1,65 @@
 ﻿using System.Data.SqlClient;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using Xunit;
 
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
 namespace Dommel.IntegrationTests
 {
-    public class PagingTests
+    [Collection("SQL")]
+    public class PagingTests //: IAsyncLifetime
     {
-        private static string GetConnectionString()
+        private static string GetConnectionString(string databaseName = "DommelTests") =>
+            $"Server=(LocalDb)\\mssqllocaldb;Database={databaseName};Integrated Security=True";
+
+        //private SqlConnection _connection;
+
+        public async Task InitializeAsync()
         {
-            var currentDir = new DirectoryInfo(Directory.GetCurrentDirectory());
-            var fileName = Path.Combine(currentDir.Parent.Parent.Parent.FullName, "App_Data", "Dommel.mdf");
-            var conStr = "Data Source=(localdb)\\MSSQLLocalDB;Integrated Security=True;AttachDBFilename=" + fileName;
-            return conStr;
+            using (var con = new SqlConnection(GetConnectionString("tempdb")))
+            {
+                await con.OpenAsync();
+                await con.ExecuteAsync("drop database if exists [DommelTests]");
+                await con.ExecuteAsync("create database [DommelTests]");
+            }
+
+            using (var connection = new SqlConnection(GetConnectionString()))
+            {
+                await connection.OpenAsync();
+
+                await connection.ExecuteAsync("create table Products (Id int IDENTITY(1,1) PRIMARY KEY, Name varchar(255) not null)");
+                await connection.InsertAsync(new Product { Name = "Chai" });
+                await connection.InsertAsync(new Product { Name = "Chang" });
+                await connection.InsertAsync(new Product { Name = "Aniseed Syrup" });
+                await connection.InsertAsync(new Product { Name = "Chef Anton's Cajun Seasoning" });
+                await connection.InsertAsync(new Product { Name = "Chef Anton's Gumbo Mix" });
+
+                await connection.InsertAsync(new Product { Name = "Chai 2" });
+                await connection.InsertAsync(new Product { Name = "Chang 2" });
+                await connection.InsertAsync(new Product { Name = "Aniseed Syrup 2" });
+                await connection.InsertAsync(new Product { Name = "Chef Anton's Cajun Seasoning 2" });
+                await connection.InsertAsync(new Product { Name = "Chef Anton's Gumbo Mix 2" });
+
+                await connection.InsertAsync(new Product { Name = "Chai 3" });
+                await connection.InsertAsync(new Product { Name = "Chang 3" });
+                await connection.InsertAsync(new Product { Name = "Aniseed Syrup 3" });
+            }
+        }
+
+        public Task DisposeAsync()
+        {
+            //_connection?.Dispose();
+            return Task.CompletedTask;
         }
 
         [Fact]
-        public void Fetches_FirstPage()
+        public async Task Fetches_FirstPage()
         {
+            await InitializeAsync();
             using (var con = new SqlConnection(GetConnectionString()))
             {
-                var paged = con.GetPaged<Product>(1, 5).ToArray();
+                var paged = (await con.GetPagedAsync<Product>(1, 5)).ToArray();
                 Assert.Equal(5, paged.Length);
                 Assert.Collection(paged,
                     p => Assert.Equal("Chai", p.Name),
@@ -33,11 +71,12 @@ namespace Dommel.IntegrationTests
         }
 
         [Fact]
-        public void Fetches_SecondPage()
+        public async Task Fetches_SecondPage()
         {
+            //await InitializeAsync();
             using (var con = new SqlConnection(GetConnectionString()))
             {
-                var paged = con.GetPaged<Product>(2, 5).ToArray();
+                var paged = (await con.GetPagedAsync<Product>(2, 5)).ToArray();
                 Assert.Equal(5, paged.Length);
             }
         }
@@ -45,6 +84,7 @@ namespace Dommel.IntegrationTests
         [Fact]
         public async Task Fetches_ThirdPartialPage()
         {
+            //await InitializeAsync();
             using (var con = new SqlConnection(GetConnectionString()))
             {
                 var paged = (await con.GetPagedAsync<Product>(3, 5)).ToArray();
@@ -55,6 +95,7 @@ namespace Dommel.IntegrationTests
         [Fact]
         public async Task SelectPaged_FetchesFirstPage()
         {
+            //await InitializeAsync();
             using (var con = new SqlConnection(GetConnectionString()))
             {
                 var paged = (await con.SelectPagedAsync<Product>(p => p.Name == "Chai", 1, 5)).ToArray();
