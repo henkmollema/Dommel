27b0b4b5b521d42d014a7a214b47056b2227da42
Henk Mollema <henkmollema@gmail.com>
2018-11-14 20:29:17 +0100

    Add non-async auto-multi-mapped Get methods

--- test/Dommel.IntegrationTests/AutoMultiMapTests.cs
+++ test/Dommel.IntegrationTests/AutoMultiMapTests.cs
@@ -24,7 +24,54 @@ namespace Dommel.IntegrationTests
         }
 
         [Fact]
-        public async Task OneToOne()
+        public void OneToOne()
+        {
+            using (var con = GetConnection())
+            {
+                var product = con.Get<Product, Category, Product>(1);
+                Assert.NotNull(product);
+                Assert.NotNull(product.Category);
+                Assert.Equal("Food", product.Category.Name);
+                Assert.Equal(product.CategoryId, product.Category.Id);
+            }
+        }
+
+        [Fact]
+        public void OneToOneNotExisting()
+        {
+            using (var con = GetConnection())
+            {
+                var product = con.Get<Product, Category, Product>(11);
+                Assert.NotNull(product);
+                Assert.Null(product.Category);
+            }
+        }
+
+        [Fact]
+        public void OneToMany()
+        {
+            using (var con = GetConnection())
+            {
+                var order = con.Get<Order, OrderLine, Order>(1);
+                Assert.NotNull(order);
+                Assert.NotNull(order.OrderLines);
+                Assert.Equal(3, order.OrderLines.Count);
+            }
+        }
+
+        [Fact]
+        public void OneToManyNonExsting()
+        {
+            using (var con = GetConnection())
+            {
+                var order = con.Get<Order, OrderLine, Order>(2);
+                Assert.NotNull(order);
+                Assert.Null(order.OrderLines);
+            }
+        }
+
+        [Fact]
+        public async Task OneToOneAsync()
         {
             using (var con = GetConnection())
             {
@@ -37,7 +84,18 @@ namespace Dommel.IntegrationTests
         }
 
         [Fact]
-        public async Task OneToMany()
+        public async Task OneToOneNotExistingAsync()
+        {
+            using (var con = GetConnection())
+            {
+                var product = await con.GetAsync<Product, Category, Product>(11);
+                Assert.NotNull(product);
+                Assert.Null(product.Category);
+            }
+        }
+
+        [Fact]
+        public async Task OneToManyAsync()
         {
             using (var con = GetConnection())
             {
@@ -45,6 +103,17 @@ namespace Dommel.IntegrationTests
                 Assert.NotNull(order);
                 Assert.NotNull(order.OrderLines);
                 Assert.Equal(3, order.OrderLines.Count);
+            }
+        }
+
+        [Fact]
+        public async Task OneToManyNonExstingAsync()
+        {
+            using (var con = GetConnection())
+            {
+                var order = await con.GetAsync<Order, OrderLine, Order>(2);
+                Assert.NotNull(order);
+                Assert.Null(order.OrderLines);
             }
         }
     }
