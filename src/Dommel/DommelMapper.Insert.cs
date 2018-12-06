95d55f13bd9acf68de6b72fcf8dda7dd4516ae2c
Damien Law <damienlaw@live.com>
2018-12-06 18:50:48 +0800

    Re-order Using statement.

--- src/Dommel/DommelMapper.Insert.cs
+++ src/Dommel/DommelMapper.Insert.cs
@@ -1,10 +1,10 @@
-using Dapper;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace Dommel
 {
