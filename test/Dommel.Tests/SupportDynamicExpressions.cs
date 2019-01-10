c109c67672638b9de97a9bac281960afd474d2ef
donkie <linjiacheng@gmail.com>
2019-01-10 18:58:52 +0800

    Support for IN-clause (#103)
    Adds support for translating contains-expressions to SQL queries with an IN-clause.

--- test/Dommel.Tests/SupportDynamicExpressions.cs
+++ test/Dommel.Tests/SupportDynamicExpressions.cs
@@ -2,6 +2,8 @@
 using Moq;
 using Moq.Dapper;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
 using System.Linq;
@@ -72,10 +74,46 @@ namespace Dommel.Tests
             Assert.Equal(2, dynamicParameters.Get<int>("p2"));
         }
 
+        [Fact]
+        public void InExpression()
+        {
+            var ids = new[] {1, 2};
+            var idList = new ArrayList {"1", "2"};
+            var guid1 = Guid.Parse("11111111-1111-1111-1111-111111111111");
+            var guid2 = Guid.Parse("22222222-2222-2222-2222-222222222222");
+            var guidList = new List<Guid> {guid1, guid2};
+            var decimalList = new List<decimal> {1.0m, 2.0m};
+            Expression<Func<Foo, bool>> expression = p =>
+                ids.Contains(p.Id) || idList.Contains(p.StringId) ||
+                decimalList.Contains(p.DecimalId) || guidList.Contains(p.Guid) ||
+                p.Bar.Contains("testIn");
+
+            var dommelExpression = sqlExpression.Where(expression);
+            var sql = dommelExpression.ToSql(out var dynamicParameters);
+
+            Assert.Equal("where [Id] in (@p1,@p2) or [StringId] in (@p3,@p4) or [DecimalId] in (@p5,@p6) or [Guid] in (@p7,@p8) or [Bar] like @p9", sql.Trim());
+            Assert.Equal(1, dynamicParameters.Get<int>("p1"));
+            Assert.Equal(2, dynamicParameters.Get<int>("p2"));
+            Assert.Equal("1", dynamicParameters.Get<string>("p3"));
+            Assert.Equal("2", dynamicParameters.Get<string>("p4"));
+            Assert.Equal(1.0m, dynamicParameters.Get<decimal>("p5"));
+            Assert.Equal(2.0m, dynamicParameters.Get<decimal>("p6"));
+            Assert.Equal(guid1, dynamicParameters.Get<Guid>("p7"));
+            Assert.Equal(guid2, dynamicParameters.Get<Guid>("p8"));
+            Assert.Equal("%testIn%", dynamicParameters.Get<string>("p9"));
+
+        }
+
         [Table("tblFoo")]
         public class Foo
         {
             public int Id { get; set; }
+
+            public string StringId { get; set; }
+
+            public decimal DecimalId { get; set; }
+
+            public Guid Guid { get; set; }
 
             public string Bar { get; set; }
         }
