d72c9b22eadfd94a2fa16c1e8e138c387dc959fe
Henk Mollema <henkmollema@gmail.com>
2019-03-30 17:23:37 +0100

    Non-async method and more tests

--- test/Dommel.IntegrationTests/FromTests.cs
+++ test/Dommel.IntegrationTests/FromTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -12,44 +9,83 @@ namespace Dommel.IntegrationTests
     {
         [Theory]
         [ClassData(typeof(DatabaseTestData))]
-        public void Sample(DatabaseDriver database)
+        public void SelectAllSync(DatabaseDriver database)
         {
             using (var con = database.GetConnection())
             {
-                _ = con.GetAll<Product>();
+                var products = con.From<Product>(sql => sql.Select());
+                Assert.NotEmpty(products);
             }
         }
 
         [Theory]
         [ClassData(typeof(DatabaseTestData))]
-        public async Task SampleAsync(DatabaseDriver database)
+        public void SelectProjectSync(DatabaseDriver database)
+        {
+            using (var con = database.GetConnection())
+            {
+                var products = con.From<Product>(sql =>
+                    sql.Select(p => new { p.Id, p.Name }));
+                Assert.NotEmpty(products);
+                Assert.All(products, p => Assert.Equal(0, p.CategoryId));
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(DatabaseTestData))]
+        public async Task SelectAll(DatabaseDriver database)
+        {
+            using (var con = database.GetConnection())
+            {
+                var products = await con.FromAsync<Product>(sql => sql.Select());
+                Assert.NotEmpty(products);
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(DatabaseTestData))]
+        public async Task SelectProject(DatabaseDriver database)
+        {
+            using (var con = database.GetConnection())
+            {
+                var products = await con.FromAsync<Product>(sql =>
+                    sql.Select(p => new { p.Id, p.Name }));
+                Assert.NotEmpty(products);
+                Assert.All(products, p => Assert.Equal(0, p.CategoryId));
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(DatabaseTestData))]
+        public async Task Select_Where(DatabaseDriver database)
         {
             using (var con = database.GetConnection())
             {
                 var products = await con.FromAsync<Product>(
                     sql => sql.Select(p => new { p.Name, p.CategoryId })
-                        .Where(p => p.Name.StartsWith("Chai"))
-                        .OrderBy(p => p.CategoryId));
+                        .Where(p => p.Name.StartsWith("Chai")));
 
                 Assert.NotEmpty(products);
+                Assert.All(products, p => Assert.StartsWith("Chai", p.Name));
             }
         }
 
         [Theory]
         [ClassData(typeof(DatabaseTestData))]
-        public async Task SampleAsync2(DatabaseDriver database)
+        public async Task KitchenSink(DatabaseDriver database)
         {
             using (var con = database.GetConnection())
             {
-                var products = await con.FromAsync<Product>(sql => 
+                var products = await con.FromAsync<Product>(sql =>
                     sql.Select(p => new { p.Name, p.CategoryId })
                         .Where(p => p.Name.StartsWith("Chai") && p.CategoryId == 1)
                         .OrWhere(p => p.Name != null)
+                        .AndWhere(p => p.CategoryId != 0)
                         .OrderBy(p => p.CategoryId)
                         .OrderByDescending(p => p.Name)
                         .Page(1, 5));
 
-                Assert.NotEmpty(products);
+                Assert.Equal(5, products.Count());
             }
         }
     }
