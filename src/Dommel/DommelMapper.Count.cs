70c3f076b5573e1bc714520acc56feff15a38108
Henk Mollema <henkmollema@gmail.com>
2019-04-08 16:19:01 +0200

    Add Count overload without select expression

--- src/Dommel/DommelMapper.Count.cs
+++ src/Dommel/DommelMapper.Count.cs
@@ -9,6 +9,34 @@ namespace Dommel
     public static partial class DommelMapper
     {
         /// <summary>
+        /// Returns the number of all entities.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="connection">The connection to the database. This can either be open or closed.</param>
+        /// <param name="transaction">Optional transaction for the command.</param>
+        /// <returns>The number of entities matching the specified predicate.</returns>
+        public static long Count<TEntity>(this IDbConnection connection, IDbTransaction transaction = null)
+        {
+            var sql = BuildCountAllSql(GetSqlBuilder(connection), typeof(TEntity));
+            LogQuery<TEntity>(sql);
+            return connection.ExecuteScalar<long>(sql, transaction);
+        }
+
+        /// <summary>
+        /// Returns the number of all entities.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="connection">The connection to the database. This can either be open or closed.</param>
+        /// <param name="transaction">Optional transaction for the command.</param>
+        /// <returns>The number of entities matching the specified predicate.</returns>
+        public static Task<long> CountAsync<TEntity>(this IDbConnection connection, IDbTransaction transaction = null)
+        {
+            var sql = BuildCountAllSql(GetSqlBuilder(connection), typeof(TEntity));
+            LogQuery<TEntity>(sql);
+            return connection.ExecuteScalarAsync<long>(sql, transaction);
+        }
+
+        /// <summary>
         /// Returns the number of entities matching the specified predicate.
         /// </summary>
         /// <typeparam name="TEntity">The type of the entity.</typeparam>
@@ -18,7 +46,7 @@ namespace Dommel
         /// <returns>The number of entities matching the specified predicate.</returns>
         public static long Count<TEntity>(this IDbConnection connection, Expression<Func<TEntity, bool>> predicate, IDbTransaction transaction = null)
         {
-            var sql = BuildCountSql(connection, predicate, out var parameters);
+            var sql = BuildCountSql(GetSqlBuilder(connection), predicate, out var parameters);
             LogQuery<TEntity>(sql);
             return connection.ExecuteScalar<long>(sql, parameters, transaction);
         }
@@ -33,23 +61,27 @@ namespace Dommel
         /// <returns>The number of entities matching the specified predicate.</returns>
         public static Task<long> CountAsync<TEntity>(this IDbConnection connection, Expression<Func<TEntity, bool>> predicate, IDbTransaction transaction = null)
         {
-            var sql = BuildCountSql(connection, predicate, out var parameters);
+            var sql = BuildCountSql(GetSqlBuilder(connection), predicate, out var parameters);
             LogQuery<TEntity>(sql);
             return connection.ExecuteScalarAsync<long>(sql, parameters, transaction);
         }
 
-        private static string BuildCountSql<TEntity>(IDbConnection connection, Expression<Func<TEntity, bool>> predicate, out DynamicParameters parameters)
+        internal static string BuildCountAllSql(ISqlBuilder sqlBuilder, Type type)
         {
-            var sqlBuilder = GetSqlBuilder(connection);
-            var type = typeof(TEntity);
             var cacheKey = new QueryCacheKey(QueryCacheType.Count, sqlBuilder, type);
             if (!QueryCache.TryGetValue(cacheKey, out var sql))
             {
-                var tableName = Resolvers.Table(type, connection);
+                var tableName = Resolvers.Table(type, sqlBuilder);
                 sql = $"select count(*) from {tableName}";
                 QueryCache.TryAdd(cacheKey, sql);
             }
 
+            return sql;
+        }
+
+        internal static string BuildCountSql<TEntity>(ISqlBuilder sqlBuilder, Expression<Func<TEntity, bool>> predicate, out DynamicParameters parameters)
+        {
+            var sql = BuildCountAllSql(sqlBuilder, typeof(TEntity));
             sql += new SqlExpression<TEntity>(sqlBuilder)
                 .Where(predicate)
                 .ToSql(out parameters);
