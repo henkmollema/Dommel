a7d4bf4722e5cfc3a7496bbca1b6924f00cd780a
Henk Mollema <henkmollema@gmail.com>
2019-12-06 16:14:13 +0100

    Pass JSON serializer options
    Allow trailing commas and skip comments in JSON strings

--- src/Dommel.Json/JsonObjectTypeHandler.cs
+++ src/Dommel.Json/JsonObjectTypeHandler.cs
@@ -7,15 +7,21 @@ namespace Dommel.Json
 {
     internal class JsonObjectTypeHandler : SqlMapper.ITypeHandler
     {
+        private static readonly JsonSerializerOptions JsonOptions = new JsonSerializerOptions
+        {
+            AllowTrailingCommas = true,
+            ReadCommentHandling = JsonCommentHandling.Skip,
+        };
+
         public void SetValue(IDbDataParameter parameter, object? value)
         {
             parameter.Value = value is null || value is DBNull
                 ? (object)DBNull.Value
-                : JsonSerializer.Serialize(value);
+                : JsonSerializer.Serialize(value, JsonOptions);
             parameter.DbType = DbType.String;
         }
 
         public object? Parse(Type destinationType, object? value) =>
-            value is string str ? JsonSerializer.Deserialize(str, destinationType) : null;
+            value is string str ? JsonSerializer.Deserialize(str, destinationType, JsonOptions) : null;
     }
 }
