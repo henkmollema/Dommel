7a236fe7939f54cedbfb5f5caa054292f77dc8da
Henk Mollema <henkmollema@gmail.com>
2020-01-07 21:30:40 +0100

    Add Codecov integration (#176)

--- build.ps1
+++ build.ps1
@@ -15,6 +15,8 @@ if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
 exec { & dotnet restore }
 
+#
+# Determine version numbers
 $branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
 $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
@@ -24,12 +26,44 @@ echo "build: Build version suffix is $buildSuffix"
 
 exec { & dotnet build Dommel.sln -c Release --version-suffix=$buildSuffix /p:CI=true }
 
+#
+# Execute tests
 echo "build: Executing tests"
 exec { & dotnet test test/Dommel.Tests -c Release --no-build }
 exec { & dotnet test test/Dommel.IntegrationTests -c Release --no-build }
 exec { & dotnet test test/Dommel.Json.Tests -c Release --no-build }
 exec { & dotnet test test/Dommel.Json.IntegrationTests -c Release --no-build }
 
+echo "build: Calculating code coverage metrics"
+
+#
+# Test coverage for Dommel
+
+# Create the first coverage in the coverlet JSON format to allow merging
+exec { & dotnet test test/Dommel.Tests -c Release -f netcoreapp3.1 --no-build /p:CollectCoverage=true }
+
+# Merge this coverage output with the previous coverage output, this time
+# create a report using the opencover format which codecov can parse
+Push-Location -Path "test/Dommel.IntegrationTests"
+exec { & dotnet test -c Release -f netcoreapp3.1 --no-build /p:CollectCoverage=true /p:MergeWith="..\Dommel.Tests\coverage.netcoreapp3.1.json" /p:CoverletOutputFormat=opencover }
+if ($env:APPVEYOR_BUILD_NUMBER) {
+    exec { & codecov -f "coverage.netcoreapp3.1.opencover.xml" }
+}
+Pop-Location
+
+#
+# Test coverage for Dommel.Json
+exec { & dotnet test test/Dommel.Json.Tests -c Release -f netcoreapp3.1 --no-build /p:CollectCoverage=true /p:Include="[Dommel.Json]*" }
+
+Push-Location -Path "test/Dommel.Json.IntegrationTests"
+exec { & dotnet test -c Release -f netcoreapp3.1 --no-build /p:CollectCoverage=true /p:Include="[Dommel.Json]*" /p:MergeWith="..\Dommel.Json.Tests\coverage.netcoreapp3.1.json" /p:CoverletOutputFormat=opencover }
+if ($env:APPVEYOR_BUILD_NUMBER) {
+    exec { & codecov -f "coverage.netcoreapp3.1.opencover.xml" }
+}
+Pop-Location
+
+#
+# Create artifcats
 if ($env:APPVEYOR_BUILD_NUMBER) {
     $versionSuffix = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10)
 }
