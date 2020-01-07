8ff8b575041841ea7b541603b9566560f6298754
Henk Mollema <henkmollema@gmail.com>
2020-01-07 15:58:32 +0100

    Remove 'DommelMapper' from file names

---
+++ src/Dommel/DefaultPropertyResolver.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Dommel
+{
+    public static partial class DommelMapper
+    {
+        /// <summary>
+        /// Default implemenation of the <see cref="IPropertyResolver"/> interface.
+        /// </summary>
+        public class DefaultPropertyResolver : IPropertyResolver
+        {
+            private static readonly HashSet<Type> PrimitiveTypesSet = new HashSet<Type>
+            {
+                typeof(object),
+                typeof(string),
+                typeof(Guid),
+                typeof(decimal),
+                typeof(double),
+                typeof(float),
+                typeof(DateTime),
+                typeof(DateTimeOffset),
+                typeof(TimeSpan),
+                typeof(byte[])
+            };
+
+            /// <summary>
+            /// Resolves the properties to be mapped for the specified type.
+            /// </summary>
+            /// <param name="type">The type to resolve the properties to be mapped for.</param>
+            /// <returns>A collection of <see cref="PropertyInfo"/>'s of the <paramref name="type"/>.</returns>
+            public virtual IEnumerable<PropertyInfo> ResolveProperties(Type type) =>
+                FilterComplexTypes(type.GetRuntimeProperties()).Where(p => p.GetSetMethod() is object && !p.IsDefined(typeof(IgnoreAttribute)));
+
+            /// <summary>
+            /// Gets a collection of types that are considered 'primitive' for Dommel but are not for the CLR.
+            /// Override this to specify your own set of types.
+            /// </summary>
+            protected virtual HashSet<Type> PrimitiveTypes => PrimitiveTypesSet;
+
+            /// <summary>
+            /// Filters the complex types from the specified collection of properties.
+            /// </summary>
+            /// <param name="properties">A collection of properties.</param>
+            /// <returns>The properties that are considered 'primitive' of <paramref name="properties"/>.</returns>
+            protected virtual IEnumerable<PropertyInfo> FilterComplexTypes(IEnumerable<PropertyInfo> properties)
+            {
+                foreach (var property in properties)
+                {
+                    var type = property.PropertyType;
+                    type = Nullable.GetUnderlyingType(type) ?? type;
+                    if (type.GetTypeInfo().IsPrimitive || type.GetTypeInfo().IsEnum || PrimitiveTypes.Contains(type))
+                    {
+                        yield return property;
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Specifies that a property should be ignored.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class IgnoreAttribute : Attribute
+    {
+    }
+}
