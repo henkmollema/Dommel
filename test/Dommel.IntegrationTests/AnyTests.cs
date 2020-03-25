cef3e1b91fe761820f047933945a011ea451d33a
Lucas Silva Chaves <lucassilvachaves@live.com>
2020-03-25 19:50:15 -0300

    Added Any Method
    This change allows checking if there's any register on database, matching a predicate

---
+++ test/Dommel.IntegrationTests/AnyTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Dommel.IntegrationTests
+{
+    [Collection("Database")]
+    public class AnyTests
+    {
+        [Theory]
+        [ClassData(typeof(DatabaseTestData))]
+        public void Any(DatabaseDriver database)
+        {
+            using var con = database.GetConnection();
+            Assert.True(con.Any<Product>());
+        }
+
+        [Theory]
+        [ClassData(typeof(DatabaseTestData))]
+        public async Task AnyAsync(DatabaseDriver database)
+        {
+            using var con = database.GetConnection();
+            Assert.True(await con.AnyAsync<Product>());
+        }
+
+        [Theory]
+        [ClassData(typeof(DatabaseTestData))]
+        public void AnyWithPredicate(DatabaseDriver database)
+        {
+            using var con = database.GetConnection();
+            Assert.True(con.Any<Product>(x => x.Name != null));
+        }
+
+        [Theory]
+        [ClassData(typeof(DatabaseTestData))]
+        public async Task AnyAsyncWithPredicate(DatabaseDriver database)
+        {
+            using var con = database.GetConnection();
+            Assert.True(await con.AnyAsync<Product>(x => x.Name != null));
+        }
+    }
+}
---
+++ test/Dommel.Tests/AnyTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using static Dommel.DommelMapper;
+
+namespace Dommel.Tests
+{
+    public class AnyTests
+    {
+        private static readonly ISqlBuilder SqlBuilder = new SqlServerSqlBuilder();
+
+        [Fact]
+        public void GeneratesAnyAllSql()
+        {
+            var sql = BuildAnyAllSql(SqlBuilder, typeof(Foo));
+            Assert.Equal("select 1 from [Foos] limit 1", sql);
+        }
+
+        [Fact]
+        public void GeneratesAnySql()
+        {
+            var sql = BuildAnySql<Foo>(SqlBuilder, x => x.Bar == "Baz", out var parameters);
+            Assert.Equal("select 1 from [Foos] where ([Bar] = @p1) limit 1", sql);
+            Assert.Single(parameters.ParameterNames);
+        }
+
+        private class Foo
+        {
+            public string? Bar { get; set; }
+        }
+    }
+}
