b46645b9a83c5bf143dcc15e50989eb0229927ec
Henk Mollema <henkmollema@gmail.com>
2021-03-31 20:41:54 +0200

    Allow reading numbers from strings in JSON

--- src/Dommel.Json/JsonObjectTypeHandler.cs
+++ src/Dommel.Json/JsonObjectTypeHandler.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 using System.Data;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Dapper;
 
 namespace Dommel.Json
 {
     internal class JsonObjectTypeHandler : SqlMapper.ITypeHandler
     {
-        private static readonly JsonSerializerOptions JsonOptions = new JsonSerializerOptions
+        private static readonly JsonSerializerOptions JsonOptions = new()
         {
             AllowTrailingCommas = true,
             ReadCommentHandling = JsonCommentHandling.Skip,
+            NumberHandling = JsonNumberHandling.AllowReadingFromString,
         };
 
         public void SetValue(IDbDataParameter parameter, object? value)
         {
             parameter.Value = value is null || value is DBNull
-                ? (object)DBNull.Value
+                ? DBNull.Value
                 : JsonSerializer.Serialize(value, JsonOptions);
             parameter.DbType = DbType.String;
         }
