a76cc42b371057d0e4c425a7a2e0a7046b58bfb8
Hasan Cem Cerit <hasan.cerit@gmail.com>
2021-07-26 15:52:00 -0400

    add test case

--- test/Dommel.Tests/SqlExpressions/SqlExpressionTests.cs
+++ test/Dommel.Tests/SqlExpressions/SqlExpressionTests.cs
@@ -12,5 +12,14 @@ namespace Dommel.Tests
             Assert.Equal(" where ([Name] = @p1)", sql);
             Assert.Equal(sql, _sqlExpression.ToString());
         }
+
+        [Fact]
+        public void ToStringVisitation_ReturnsSql()
+        {
+            var _sqlExpression = new SqlExpression<Product>(new SqlServerSqlBuilder());
+            var sql = _sqlExpression.Where(p => p.CategoryId.ToString() == "1").ToSql();
+            Assert.Equal(" where (CAST([CategoryId] AS CHAR) = @p1)", sql);
+            Assert.Equal(sql, _sqlExpression.ToString());
+        }
     }
 }
