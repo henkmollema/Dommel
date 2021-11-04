f93a6869838e0125e48b2e6669074dcf5a6d6217
Henk Mollema <henkmollema@gmail.com>
2021-11-04 14:33:42 +0100

    Fix Linux build

--- build.sh
+++
@@ -1,6 +0,0 @@
-dotnet restore
-dotnet build
-dotnet test test/Dommel.Tests --no-build
-dotnet test test/Dommel.IntegrationTests --no-build
-dotnet test test/Dommel.Json.Tests --no-build
-dotnet test test/Dommel.Json.IntegrationTests --no-build
